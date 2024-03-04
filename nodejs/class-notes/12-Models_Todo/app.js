ee3cfc0bcb75d67c15552624a72264e3b4497b25
batumzff <batuhanmuzafferoglu@gmail.com>
2024-03-04 22:00:10 +0300

    db connection

--- nodejs/class-notes/12-Models_Todo/app.js
+++ nodejs/class-notes/12-Models_Todo/app.js
@@ -13,12 +13,72 @@ const PORT = process.env.PORT || 8000;
 // Accept json data:
 app.use(express.json())
 
-app.all('/', (req, res) => {
+// Catch async-errors:
+require('express-async-errors')
+
+app.all('/abc', (req, res) => { // Allow all methods. all -> URL=/ - use -> URL=/*
     res.send('WELCOME TO TODO API')
 })
+/* ------------------------------------------------------- */
 
-// continue from here...
+const { Sequelize, DataTypes } = require('sequelize')
+// sequelize instance oluştur:
+const sequelize = new Sequelize('sqlite:./db.sqlite3')
 
+// define methodu sequelize modeli oluşturur:
+// her bir model, veritabanında bir tabloya denk gelir.
+// sequelize.define('tableName', {  modelDetails  })
+
+const Todo = sequelize.define('todos', {
+
+    // ilk sutun olarak id sutunu sequelize tarafından otomatik oluşturulur/yönetilir.
+    // id: {
+    //     type: DataTypes.INTEGER,
+    //     allowNull: false, // default: true
+    //     unique: true, // default: false
+    //     comment: 'description',
+    //     primaryKey: true, // default: false
+    //     autoIncrement: true, // default: false
+    //     field: 'custom_name',
+    //     defaultValue: 'default', // default: null
+    // },
+
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    description: DataTypes.TEXT, // ShortHand Using.
+
+    priority: { // -1: Low, 0: Norm, 1: High
+        type: DataTypes.TINYINT,
+        allowNull: false,
+        default: 0
+    },
+
+    isDone: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        default: false
+    }
+
+    //? Not need define createdAt & updatedAt fields.
+    //? createdAt ve updatedAt tanımlamaya gerek yoktur. Sequelize otomatik oluşturur/yönetir.
+})
+
+// Syncronization:
+// Model bilgilerini db'ye uygula:
+// sequelize.sync() // CREATE TABLE
+// sequelize.sync({ force: true }) // DROP TABLE & CREATE TABLE
+// sequelize.sync({ alter: true }) // TO BACKUP & DROP TABLE & CREATE TABLE & FROM BACKUP
+
+// Connect to db:
+sequelize.authenticate()
+    .then(() => console.log('* DB Connected *'))
+    .catch(() => console.log('* DB Not Connected *'))
+
+
+/* ------------------------------------------------------- */
 const errorHandler = (err, req, res, next) => {
     const errorStatusCode = res.errorStatusCode ?? 500
     console.log('errorHandler worked.')
