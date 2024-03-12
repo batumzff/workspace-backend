030efd033c39636675414a0c68570553f8370792
batumzff <batuhanmuzafferoglu@gmail.com>
2024-03-12 22:25:06 +0300

    up

---
+++ nodejs/projects/todo-fullstack/backend/models/todo.model.js
@@ -0,0 +1,42 @@
+"use strict";
+const mongoose = require("mongoose");
+
+const PRIORITIES = {
+  LOW: "low",
+  NORMAL: "normal",
+  HIGH: "high",
+};
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    isDone: {
+      type: Boolean,
+      default: false,
+    },
+    priority: {
+      //low, normal,high
+      type: String,
+      enum: {
+        values: Object.values(PRIORITIES),
+        message: "low,normal, high could be select",
+      },
+    },
+  },
+  {
+    collection: "todo",
+    timestamps: true,
+  },
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+module.exports = { Todo };
