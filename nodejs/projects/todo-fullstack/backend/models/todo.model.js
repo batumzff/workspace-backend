2b1d2995c09ffee97474a0e6c8f768a19182a8a5
batumzff <batuhanmuzafferoglu@gmail.com>
2024-03-13 18:54:31 +0300

    UP

--- nodejs/projects/todo-fullstack/backend/models/todo.model.js
+++ nodejs/projects/todo-fullstack/backend/models/todo.model.js
@@ -39,5 +39,14 @@ const todoSchema = new mongoose.Schema(
   },
 );
 
+todoSchema.set("toJSON", {
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+    // ret.createdAt = ret.createdAt.toLocaleString("tr-tr");
+  },
+});
+
 const Todo = mongoose.model("Todo", todoSchema);
 module.exports = { Todo };
