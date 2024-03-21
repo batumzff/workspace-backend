84de94887ebf5b3ff87f44f72f06046742b4d11c
batumzff <batuhanmuzafferoglu@gmail.com>
2024-03-21 21:59:59 +0300

    permission update

--- nodejs/class-notes/PersonnelAPI/src/routes/department.router.js
+++ nodejs/class-notes/PersonnelAPI/src/routes/department.router.js
@@ -1,25 +1,28 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
 
-const department = require('../controllers/department.controller')
+const department = require("../controllers/department.controller");
+const permissions = require("../middlewares/permissions");
 
 // URL: /departments
 
-router.route('/')
-    .get(department.list)
-    .post(department.create)
+router
+  .route("/")
+  .get(permissions.isLogin, department.list)
+  .post(permissions.isAdmin, department.create);
 
-router.route('/:id')
-    .get(department.read)
-    .put(department.update)
-    .patch(department.update)
-    .delete(department.delete)
+router
+  .route("/:id")
+  .get(permissions.isLogin, department.read)
+  .put(permissions.isAdmin, department.update)
+  .patch(permissions.isAdmin, department.update)
+  .delete(permissions.isAdmin, department.delete);
 
-router.get('/:id/personnels', department.personnels)
+router.get("/:id/personnels", permissions.isAdminOrLead, department.personnels);
 
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
